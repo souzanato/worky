06b9d1e8c0191546282c5e8da0d2da92c2180a7a
Renato Carvalho de Souza <renato.souza@economia.gov.br>
2025-10-08 01:37:37 -0300

    Adicionando turbo stream ao prompt generator

--- config/routes.rb
+++ config/routes.rb
@@ -58,5 +58,7 @@ Rails.application.routes.draw do
   # get "service-worker": "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
+  mount ActionCable.server => "/cable"
+
   root "home#index"
 end
